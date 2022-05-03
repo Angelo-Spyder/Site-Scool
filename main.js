26173fc16cb1bff5e7c35feb6112f9d53b571d2f
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-05-03 17:59:26 -0300

    Criando interatividade de javascript e efeitos css na imagem com conteúdos html

---
+++ main.js
@@ -0,0 +1,41 @@
+
+let campoImagem = document.querySelector(".campo-imagem");
+let bolinhas = document.querySelectorAll(".bola");
+
+let imagens= [
+    {
+        img: "images/paisagem-1.jpg"
+    },
+    {
+        img: "images/paisagem-2.jpg"
+    },
+    {
+        img: "images/paisagem-3.jpg"
+    },
+];
+
+
+
+function alterarImagens(){
+    let position = 0
+    let img = document.createElement("img");
+    img.classList.add("imagem-scrool")
+    setInterval(() => {
+        if(position == 3){
+            position = 0
+        }else{
+            img.classList.remove("desaparecer");
+            bolinhas[position].classList.add("bolinha-escolhida");
+            img.src = imagens[position].img;
+            campoImagem.appendChild(img);
+            setInterval(() => {
+                img.classList.add("desaparecer");
+                bolinhas[position - 1].classList.remove("bolinha-escolhida")
+            }, 2100);
+            
+            position = position + 1;
+        }
+    },4500)
+}
+
+alterarImagens();
